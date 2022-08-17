cd98528a41c3bb78aef5cddc0271da8f2ec5f118
Gustavo-Carneiro-Alves <80040530+Gustavo-Carneiro-Alves@users.noreply.github.com>
2022-08-16 21:31:20 -0300

    Bhaskara e PessoaBrasileira adicionados

---
+++ 2PessoaBrasileira/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Uma pessoa só pode votar em eleições brasileiras se ela for maior que 16 anos e for cidadã brasileira. 
+Crie um programa com duas variáveis, int idade, bool brasileira, 
+e faça com que o programa diga se a pessoa está apta a votar ou não, de acordo com os dados nas variáveis.
+*/
+
+
+Console.WriteLine("Insira sua idade!");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Você é brasileiro? S/N");
+string booleano = Console.ReadLine();
+bool brasileira;
+
+if (booleano == "S" | booleano== "s"){
+    brasileira = true;
+}
+else{
+    brasileira = false;
+}
+
+if (idade>16 & brasileira ==true){
+    Console.WriteLine("Você pode votar");
+}
+else{
+    Console.WriteLine("Você NÃO pode votar");
+}
